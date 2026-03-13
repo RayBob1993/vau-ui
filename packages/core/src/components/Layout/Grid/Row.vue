241b652934285bf01cc0f6428083120c7f844ad7
@turkov <pautov.d@office.lan>
2026-03-14 00:04:00 +0300

    feat: разработка

--- packages/core/src/components/Layout/Grid/Row.vue
+++ packages/core/src/components/Layout/Grid/Row.vue
@@ -12,7 +12,19 @@
     class="row"
     :class="{
       [`row--justify-${justify}`]: justify,
+      [`row--justify-xs-${justifyXs}`]: justifyXs,
+      [`row--justify-sm-${justifySm}`]: justifySm,
+      [`row--justify-md-${justifyMd}`]: justifyMd,
+      [`row--justify-lg-${justifyLg}`]: justifyLg,
+      [`row--justify-xl-${justifyXl}`]: justifyXl,
+      [`row--justify-xxl-${justifyXxl}`]: justifyXxl,
       [`row--align-${align}`]: align,
+      [`row--align-xs-${alignXs}`]: alignXs,
+      [`row--align-sm-${alignSm}`]: alignSm,
+      [`row--align-md-${alignMd}`]: alignMd,
+      [`row--align-lg-${alignLg}`]: alignLg,
+      [`row--align-xl-${alignXl}`]: alignXl,
+      [`row--align-xxl-${alignXxl}`]: alignXxl,
       'row--gutters-x': guttersX,
       'row--gutters-y': guttersY
     }"
